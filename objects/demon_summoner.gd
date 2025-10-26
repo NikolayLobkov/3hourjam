91faf703e15f3d40242bd6586d2a9bc1fdb631a6
Nikolay Lobkov <nikolaylobkov1272@gmail.com>
2025-10-26 22:05:39 +0300

    some changes in map collision

--- objects/demon_summoner.gd
+++ objects/demon_summoner.gd
@@ -4,11 +4,13 @@ extends Marker3D
 signal summoned(character: Character)
 
 
-const DEMON: PackedScene = preload('res://characters/demon/demon.tscn')
+const DEMON_MAG: PackedScene = preload('res://characters/demon_mag/demon_mag.tscn')
+const DEMON_MONK: PackedScene = preload('res://characters/demon_monk/demon_monk.tscn')
+const DEMON_SKELETON: PackedScene = preload('res://characters/demon_skeleton/demon_skeleton.tscn')
 
 
-func summon() -> void:
-	var demon: Demon = DEMON.instantiate()
+func summon(d: PackedScene) -> void:
+	var demon: Demon = d.instantiate()
 	get_tree().current_scene.call_deferred('add_child', demon)
 	
 	await get_tree().process_frame
