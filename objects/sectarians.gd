1dff7054df9542ea9c3cc23bf19788c4a7693fc6
Nikolay Lobkov <nikolaylobkov1272@gmail.com>
2025-10-29 21:30:43 +0300

    updated

--- objects/sectarians.gd
+++ objects/sectarians.gd
@@ -3,17 +3,12 @@ extends Node3D
 
 
 
-
-
-
 func _on_ritual_action_maked(act: String) -> void:
 	action(act)
-
 
 func action(act: String) -> void:
 	for s: Sectarian in get_children():
 		s.action(act)
-
 
 func _on_ritual_ended() -> void:
 	for s: Sectarian in get_children():
